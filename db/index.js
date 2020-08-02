79b6373456750b0935274a73db7cf326100a313c
William Joel Clavell <wjclavell28@gmail.com>
2020-08-02 13:47:20 -0400

    changed mongoURI variable

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // USING ENV URI IF AVAILABLE
 let MONGODB_URI =
   process.env.PROD_MONGODB ||
-  process.env.MONGODB_URI ||
+  process.env.MONGODB_URI + "gameRatings" ||
   "mongodb://localhost:27017/";
 console.log(MONGODB_URI);
 // cononect to db
