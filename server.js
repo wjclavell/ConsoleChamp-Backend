3044ab941e66f23726a2021111c7e88a717fdf4a
William Clavell <wjclavell28@gmail.com>
2020-08-31 23:14:27 -0400

    starting the user authentication process

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const gameRoutes = require("./routes/game");
 const criticRoutes = require("./routes/critic");
 const userRoutes = require("./routes/user");
@@ -44,6 +45,39 @@ app.use("/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("Still working? Yup!");
 });
+
+//* USER AUTH
+
+app.post("/login", authenticateToken, (req, res) => {
+  //authenticate user
+
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+});
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(""[1]);
+  if ((token = null)) return res.status(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403);
+    req.user = user;
+    next();
+  });
+};
+
+const generateAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+};
 
 // listener
 app.listen(PORT, () => {
